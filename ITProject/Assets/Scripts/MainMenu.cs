4c1e86e9a184d61c2d819b1edc928d5779065b3f
selinoe <oeztuerk.selin@hotmail.com>
2019-09-14 13:34:06 +0200

    OnBoarding; Design

--- ITProject/Assets/Scripts/MainMenu.cs
+++ ITProject/Assets/Scripts/MainMenu.cs
@@ -16,4 +16,9 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("ContinentSelection");
     }
+
+    public void OpenOnboarding()
+    {
+      SceneManager.LoadScene("OnBoarding");
+    }
 }
